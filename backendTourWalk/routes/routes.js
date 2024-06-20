33c98ac4e6cd38fc11a4a39a43b7e6b5cb11f718
DavisTorres <deivis.940@gmail.com>
2024-06-19 21:18:44 -0500

    cambios finales

--- backendTourWalk/routes/routes.js
+++ backendTourWalk/routes/routes.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const usuarioController = require("../controller/usuarioController");
-const destinoController = require("../controller/destinoController");
+const express = require('express')
+const router  = express.Router();
+const usuarioController =require('../controller/usuarioController');
+const destinoController =require('../controller/destinoController');
 
-router.post("/registrar-usuario", usuarioController.registrarUsuario);
-router.post("/login-usuario", usuarioController.loginUsuario);
-router.get("/consultar-usuario", usuarioController.consultarUsuarios);
-router.put("/actualizar-usuario", usuarioController.actualizarUsuario);
+router.post('/registrar-usuario', usuarioController.registrarUsuario);
+router.post('/login-usuario', usuarioController.loginUsuario);
+
 
 router.post('/crear-destino', destinoController.crearDestino);
 router.get('/listar-destino', destinoController.obtenerDestinos);
-router.put('/actualizar-destino', destinoController.actualizarDestino);
+router.put('/actualizar-destino/:id', destinoController.actualizarDestino);
 router.delete('/eliminar-destino', destinoController.eliminarDestino);
 
 
 
-module.exports = router;
+module.exports= router;
