96cf086b9748c92fca1a3f90877a76f440fe8757
Dallana Bastidas <cindydallana2822@gmail.com>
2024-06-21 19:48:59 -0500

    reservacontroller

--- backendTourWalk/routes/routes.js
+++ backendTourWalk/routes/routes.js
@@ -19,6 +19,6 @@ router.put('/actualizar-destino/:id', destinoController.actualizarDestino);
 router.delete('/eliminar-destino', destinoController.eliminarDestino);
 
 router.post('/crear-reserva', reservaController.crearReserva);
-router.get("/consultar-reserva", consultarReserva.consultarReserva);
+router.get("/consultar-reserva", reservaController.consultarReserva);
 
 module.exports= router;
