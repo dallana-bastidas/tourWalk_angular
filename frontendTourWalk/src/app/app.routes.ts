45ba479cc99f8744f03e2e8bba365f5ab5ec9b82
DavisTorres <deivis.940@gmail.com>
2024-06-25 20:53:17 -0500

    inicio

--- frontendTourWalk/src/app/app.routes.ts
+++ frontendTourWalk/src/app/app.routes.ts
@@ -1,3 +1,5 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
