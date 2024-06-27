2b2895601fc1fd764a71658e5745ef1b732543d0
jcast08 <jhoancamilo0812@hotmail.com>
2024-06-26 20:44:05 -0500

    ultimos cambios

--- frontendTourWalk/src/app/app.routes.ts
+++ frontendTourWalk/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent},
+    { path: '', component: FooterComponent},
+
+];
