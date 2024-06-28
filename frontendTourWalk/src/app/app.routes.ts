a9575efd849b1641f3a95df8afe674629dc0ef20
jcast08 <jhoancamilo0812@hotmail.com>
2024-06-27 21:38:20 -0500

    ultimos cambios

--- frontendTourWalk/src/app/app.routes.ts
+++ frontendTourWalk/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { DestinosComponent } from './components/destinos/destinos.component';
 import { LoginComponent } from './components/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 export const routes: Routes = [
     {path: "", component:InicioComponent},
     {path: "destinos", component:DestinosComponent},
-    {path: "ingreso", component:LoginComponent}
+    {path: "ingreso", component:LoginComponent},
+    { path: "registro", component: RegistroComponent }
 ];
 
