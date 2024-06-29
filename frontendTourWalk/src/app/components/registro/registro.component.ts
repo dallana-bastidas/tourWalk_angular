e4e687ce884142a644a62a71fd8b9b47a2b35b13
jcast08 <jhoancamilo0812@hotmail.com>
2024-06-28 21:43:59 -0500

    cambios viernes

--- frontendTourWalk/src/app/components/registro/registro.component.ts
+++ frontendTourWalk/src/app/components/registro/registro.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-registro',
-  standalone: true,
-  imports: [],
-  templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+    selector: 'app-registro',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        CommonModule,
+    ],
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+    formRegistro: FormGroup;
+    regexEmail = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
 
+    constructor(
+        private fb: FormBuilder,
+        private _apiServicio: ApiService
+    ) {
+        this.formRegistro = this.fb.group({
+            nombres: ['', Validators.required],
+            apellidos: ['', Validators.required],
+            email: ['', [Validators.required, Validators.pattern(this.regexEmail)]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmarPassword: ['', Validators.required]
+        });
+    }
+
+    passwordMatchValidator(form: FormGroup) {
+        return form.controls['password'].value === form.controls['confirmarPassword'].value ? null : { mismatch: true };
+    }
+
+    registerUsuarioSistema() {
+        if (this.formRegistro.valid) {
+            this._apiServicio.postRegistrarUsuario(this.formRegistro.value).subscribe(data => {
+                console.log(data);
+            });
+        }
+    }
 }
